93891d92d0aa7775fe09de67ede817cd060426f7
Sergio Moreno López <sergio.moreno.lopez@pm.me>
2026-02-02 14:26:26 +0100

    Initial commit for the 'Lost And Found' app backend

---
+++ src/main/java/com/fruche/lostandfoundbackend/LostAndFoundBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fruche.lostandfoundbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostAndFoundBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LostAndFoundBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fruche/lostandfoundbackend/LostAndFoundBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fruche.lostandfoundbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostAndFoundBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
