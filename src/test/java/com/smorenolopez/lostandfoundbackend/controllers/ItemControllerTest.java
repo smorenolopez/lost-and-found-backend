b99b75d8a2b0245d1cd3079eb00e22761a3c7136
Sergio Moreno López <sergio.moreno.lopez@pm.me>
2026-02-04 01:58:58 +0100

    Addition of more tests of ItemController

--- src/test/java/com/smorenolopez/lostandfoundbackend/controllers/ItemControllerTest.java
+++ src/test/java/com/smorenolopez/lostandfoundbackend/controllers/ItemControllerTest.java
@@ -7,6 +7,9 @@ import com.smorenolopez.lostandfoundbackend.services.ItemServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.ResponseEntity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -30,6 +33,27 @@ class ItemControllerTest {
         assertEquals("This is an ItemDTO test object", response.getBody().getDescription());
     }
 
+    @Test
+    void getItems_shouldReturnItemResponses() {
+        // Given
+        ItemService itemService = mock(ItemServiceImpl.class);
+        when(itemService.findAllItems()).thenReturn(this.createItemResponses());
+        ItemController itemController = new ItemController(itemService);
+
+        // When
+        ResponseEntity<List<ItemResponse>> itemResponses = itemController.getItems();
+
+        // Then
+        assertNotNull(itemResponses);
+        assertNotNull(itemResponses.getBody());
+        assertEquals(1L ,itemResponses.getBody().get(0).getId());
+        assertEquals("Item response 1",itemResponses.getBody().get(0).getDescription());
+        assertEquals(2L ,itemResponses.getBody().get(1).getId());
+        assertEquals("Item response 2" ,itemResponses.getBody().get(1).getDescription());
+        assertEquals(3L ,itemResponses.getBody().get(2).getId());
+        assertEquals("Item response 3" ,itemResponses.getBody().get(2).getDescription());
+    }
+
     private ItemDTO createItemDTO() {
         ItemDTO itemDTO = new ItemDTO();
         itemDTO.setDescription("This is a ItemDTO test object");
@@ -38,6 +62,14 @@ class ItemControllerTest {
 
     private ItemResponse createItemResponse() {
         return new ItemResponse(1L, "This is an ItemDTO test object");
+    }
+
+    private List<ItemResponse> createItemResponses() {
+        List<ItemResponse> itemResponses = new ArrayList<>();
+        for (long i = 0; i < 3; i++) {
+            itemResponses.add(new ItemResponse(i + 1, "Item response " + (i + 1)));
+        }
+        return itemResponses;
     }
 
 }
