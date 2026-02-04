d284c7890bd8d69f4f0063b2551b63e855b0298b
Sergio Moreno López <sergio.moreno.lopez@pm.me>
2026-02-04 14:12:15 +0100

    Disabled empty system test

--- src/test/java/com/smorenolopez/lostandfoundbackend/LostAndFoundBackendApplicationTests.java
+++ src/test/java/com/smorenolopez/lostandfoundbackend/LostAndFoundBackendApplicationTests.java
@@ -1,8 +1,10 @@
 package com.smorenolopez.lostandfoundbackend;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Disabled
 @SpringBootTest
 class LostAndFoundBackendApplicationTests {
 
